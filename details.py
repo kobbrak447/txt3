8138d7b257bae42eaa0943adcfbee20caeb63651
kobbrak447 <153929734+kobbrak447@users.noreply.github.com>
2023-12-16 09:10:44 +0530

    Update details.py

--- details.py
+++ details.py
@@ -3,7 +3,7 @@ import os
 api_id = 23621134
 api_hash = "3e49039179441fb424d90680ecffe365"
 bot_token = "6810217753:AAHLKtUCw_PniSc3kkiY477FsCIqvpMGDkU"
-auth_users = 1001074184
-sudo_user = 1001074184
-log_channel = -1002140049222
-txt_channel = -1002140049222
+auth_users = -1002140049222
+sudo_user = -1002140049222
+log_channel = -4099057818
+txt_channel = -4007082897
